ac883becd1d5d8f0edd35d075c844b2f9000c4e5
aschemily <aschemily@gmail.com>
2022-02-24 20:12:23 -0500

    set up

---
+++ MovieSearch/Controllers/ModelControllers/MovieController.swift
@@ -0,0 +1,15 @@
+//
+//  MovieController.swift
+//  MovieSearch
+//
+//  Created by Emily Asch on 2/24/22.
+//
+
+import Foundation
+
+class MovieController{
+    //api key: d4d46debf9af5f839e76cd13507d47ed
+    //search query: https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
+    //images query: https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg
+
+}
